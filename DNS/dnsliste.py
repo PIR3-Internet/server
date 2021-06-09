20d1895ac27256af18b72096fdd000e235fdb232
T-lorrain <72496171+T-lorrain@users.noreply.github.com>
2021-06-09 15:56:27 +0200

    DNS modif local

--- DNS/dnsliste.py
+++ DNS/dnsliste.py
@@ -1,69 +1,99 @@
 import geoip2.database
 import geoip2.errors
-import dns.query
+import dns.resolver
 import csv
 import sqlite3
-import whois
 
-name_server = '1.1.1.1'
-domainList = []
 
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
+def lectureDomain():
+    List = []
+    f = open(r"list1m2020.csv")
+    myReader = csv.reader(f)
+    for row in myReader:
+        line = row[0]
+        List.append(line)
+    List = List[:1000]
+    print(len(List))
+    return List
 
-conn.execute('''CREATE TABLE if not exists DNS
-         (URL TEXT NOT NULL,
-         NS TEXT);''')
 
-f = open(r"list1m2020.csv")
-myReader = csv.reader(f)
+def get_ns_url(domain):
+    try:
+        response = dns.resolver.resolve(domain, 'NS')
+        NSlist = [res.to_text() for res in response]
+        return NSlist
+    except dns.resolver.NoAnswer:
+        print("Pas de réponse DNS pour ce domaine : ", domain)
+    except dns.exception.Timeout:
+        print(f"Timeout for {domain}")
+    except dns.resolver.NXDOMAIN:
+        print(f"DNS query name does not exist: {domain}")
+    except dns.resolver.NoNameservers:
+        print(f"No response for: {domain}")
+    return []
 
-for row in myReader:
-    ligne = row[0]
-    domainList.append(ligne)
 
-domainList = domainList[:1000]
-print(domainList)
+def get_ip_ns(NS):
+    try:
+        result = dns.resolver.resolve(NS, 'A')
+        IP = result[0].to_text()
+        return IP
+    except dns.resolver.NoAnswer:
+        print(f"Pas de réponse IP pour dns : {NS}")
+    except dns.exception.Timeout:
+        print(f"Timeout pour DNS Request : {NS}")
+    except dns.resolver.NXDOMAIN:
+        print(f"DNS name does not exist for {NS}")
+    return None
 
-for domain in domainList:
-    orgList = []
-    orgTemp = None
-    domain = dns.name.from_text(domain)
-    alreadyHere = False
 
-    if not alreadyHere:
-        request = dns.message.make_query(domain, dns.rdatatype.NS)
-        response = dns.query.udp(request, name_server)
+def get_asn_from_ip(IP, orgList):
+    with geoip2.database.Reader('./GeoLite2-ASN/GeoLite2-ASN.mmdb') as reader:
         try:
-            NSlist = list(response.answer[0].items.keys())
-        except IndexError:
-            print("Pas de réponse DNS pour ce domaine : ", domain)
-            continue
-        # print(len(NSlist))
-        for NS in NSlist:
-            # print(NS.to_text())
-            dns_domain = dns.name.from_text(NS.to_text())
-            request_dns = dns.message.make_query(dns_domain, dns.rdatatype.A)
-            IP_response = dns.query.udp(request_dns, name_server)
-            try:
-                IPList = list(IP_response.answer[0].items.keys())
-                IP = IPList[0]
-                print(IP.to_text(), " / ", domain)
-                with geoip2.database.Reader('./GeoLite2-ASN/GeoLite2-ASN.mmdb') as reader:
-                    try:
-                        answer = reader.asn(IP.to_text())
-                        print(answer.autonomous_system_organization)
-                        if answer.autonomous_system_organization != orgTemp:
-                            orgList.append(answer.autonomous_system_organization)
-                        orgTemp = answer.autonomous_system_organization
-                    except geoip2.errors.AddressNotFoundError:
-                        print('IP not in Database')
-                    except ValueError:
-                        print("Problème avec l'IP")
-            except IndexError:
-                pass
-        for item in orgList:
-            c.execute('''INSERT INTO DNS(URL, NS) VALUES(?, ?)''', (str(domain), item))
-            conn.commit()
+            answer = reader.asn(IP)
+            print(answer.autonomous_system_organization)
+            if answer.autonomous_system_organization not in orgList:
+                orgList.append(answer.autonomous_system_organization)
+        except geoip2.errors.AddressNotFoundError:
+            print('IP not in Database')
+        except ValueError:
+            print("Problème avec l'IP")
 
-print("Fin de recherche")
+
+def insert_db(domain, orgList):
+    for item in orgList:
+        c.execute('''INSERT INTO DNS(URL, NS) VALUES(?, ?)''', (str(domain), item))
+        conn.commit()
+
+
+if __name__ == "__main__":
+
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    conn.execute('''CREATE TABLE if not exists DNS
+             (URL TEXT NOT NULL,
+             NS TEXT);''')
+
+    domainList = lectureDomain()
+    for domain in domainList:
+        orgTemp = None
+        orgList = []
+        NSlist = get_ns_url(domain)
+        if len(NSlist)!=0:
+            for NS in NSlist:
+                IP = get_ip_ns(NS)
+                if IP != None:
+                    get_asn_from_ip(IP, orgList)
+            insert_db(domain, orgList)
+
+    print("Fin de recherche")
+
+
+
+
+
+
+
+
+
--- DNS/graph.py
+++ DNS/graph.py
@@ -3,18 +3,72 @@ import matplotlib.pyplot as plt
 import difflib
 
 
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-org = {}
+def matching(name):
+    if "AMAZON" in name:
+        return "Amazon"
+    if "Alibaba" in name:
+        return "Alibaba"
+    if "CHINANET" in name:
+        return "ChinaNet"
+    if "Chinanet" in name:
+        return "ChinaNet"
+    if "CHINA UNICOM" in name:
+        return "China Unicom"
+    if "China Unicom" in name:
+        return "China Unicom"
+    if "YAHOO" in name:
+        return "Yahoo"
+    if "Huawei" in name:
+        return "Huawei"
+    return name
 
-cursor = conn.execute('''SELECT NS FROM DNS''')
-for url in cursor:
-    if url not in org:
-        org[url] = 1
-    else:
-        org[url]+=1
 
-print(org)
+def make_graph(dico):
+    labels = []
+    values = []
+    for k, v in sorted(dico.items(), key=lambda x: x[1]):
+        if v > 10:
+            labels.append((k, v))
+            values.append(v)
+        else:
+            labels.append("")
+            values.append(v)
+    plt.pie(values, labels=labels)
+    plt.show()
+
+if __name__ == "__main__":
+
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    org = {}
+
+    cursor = conn.execute('''SELECT NS FROM DNS''')
+    for url in cursor:
+        if url not in org:
+            org[url] = 1
+        else:
+            org[url] += 1
+
+    print(org)
+
+    corp = {}
+
+    for name in org.keys():
+        corp_name = matching(name[0])
+        print(corp_name)
+        if corp_name in corp.keys():
+            corp[corp_name] += org[name]
+        else:
+            corp[corp_name] = org[name]
+
+    print(corp)
+    make_graph(org)
+    make_graph(corp)
+
+
+
+
+
 
 """
 matching = []
